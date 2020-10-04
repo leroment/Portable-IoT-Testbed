b41baa594af77e3552d989d6a328369aa6b69621
Prashant Shrestha <sthprashant@gmail.com>
2020-10-05 03:54:05 +1100

    created login and register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./login-register/components/Login";
+import LoginRegister from "./login-register/pages/LoginRegister";
 
 function App() {
   return (
     <div className="App">
-      <h1> Grafana in React App</h1>
+      <Router>
+        <Route path="/">
+          <LoginRegister />
+        </Route>
+      </Router>
+      {/* <h1> Grafana in React App</h1>
       <iframe
         src="http://localhost:3000/d-solo/c8J-K7dMz/andrews-ecg-readings?orgId=1&from=1599960944380&to=1599960992217&panelId=2"
         width="100%"
         height="800"
         frameborder="0"
         title="ecg readings"
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
---
+++ frontend/src/login-register/pages/LoginRegister.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import "../main.css";
+import Login from "../components/Login";
+import SignUp from "../components/SignUp";
+import Overlay from "../components/Overlay";
+
+const LoginRegister = () => {
+  const [rightPanelActive, setRightPanelActive] = useState(false);
+
+ 
+
+  return (
+    <div className="main">
+      <div
+        className={`container ${rightPanelActive ? `right-panel-active` : ``}`}
+      >
+        <SignUp />
+        <Login />
+        <Overlay
+          handleClickSignInButton={() => {
+            console.log("clicked singin button")
+            setRightPanelActive(false)
+          }}
+          handleClickSignUpButton={() => {
+            setRightPanelActive(true)
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginRegister;
