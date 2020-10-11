e405288905791cb23ecbb5f74150d57681792e81
Prashant Shrestha <sthprashant@gmail.com>
2020-10-11 15:31:04 +1100

    fixed pages folder

---
+++ frontend/src/dashboard/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div>This is the dashboard page where you will see all the data from sensors</div>
+    </div>
+  );
+};
+
+export default Dashboard;
