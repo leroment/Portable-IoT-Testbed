9e7d5ccd2059e6d8137a162ed16d379f7ccd3c97
leroment <andrewcai19972011@gmail.com>
2020-10-11 16:19:52 +1100

    add iframe for grafana

--- frontend/src/dashboard/pages/Dashboard.js
+++ frontend/src/dashboard/pages/Dashboard.js
@@ -3,7 +3,33 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div>
-      <div>This is the dashboard page where you will see all the data from sensors</div>
+      <div>
+        This is the dashboard page where you will see all the data from sensors
+      </div>
+      <div>&nbsp;</div>
+      <iframe
+        src="http://localhost:3000/d-solo/c8J-K7dMz/andrews-ecg-readings?orgId=1&from=now-5m&to=now&refresh=5s&theme=light&panelId=4"
+        width="900"
+        height="400"
+        frameborder="0"
+        title="EDA Readings"
+      ></iframe>{" "}
+      &nbsp;
+      <iframe
+        src="http://localhost:3000/d-solo/c8J-K7dMz/andrews-ecg-readings?orgId=1&from=now-5m&to=now&refresh=5s&theme=light&panelId=2"
+        width="900"
+        height="400"
+        frameborder="0"
+        title="ECG Readings"
+      ></iframe>
+      <div>&nbsp;</div>
+      <iframe
+        src="http://localhost:3000/d-solo/c8J-K7dMz/andrews-ecg-readings?orgId=1&from=now-5m&to=now&refresh=5s&theme=light&panelId=6"
+        width="900"
+        height="400"
+        frameborder="0"
+        title="Glucose Readings"
+      ></iframe>
     </div>
   );
 };
