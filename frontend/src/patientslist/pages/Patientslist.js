e44db4c5ccb068c682d595a75f5bda3f717b5165
leroment <andrewcai19972011@gmail.com>
2020-10-18 15:21:55 +1100

    add patientlist

--- frontend/src/patientslist/pages/Patientslist.js
+++ frontend/src/patientslist/pages/Patientslist.js
@@ -3,26 +3,11 @@ import { DataGrid } from "@material-ui/data-grid";
 import axios from "axios";
 
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'username', headerName: 'Username', width: 300 },
-    { field: 'email', headerName: 'Email', width: 400 },
-    { field: 'is_staff', headerName: 'Role', width: 100 },
-]
 
-const rows = [
-    { id: 1, username: 'Snow', email: 'Jon', is_staff: 35 },
-    { id: 2, username: 'Lannister', email: 'Cersei', is_staff: 42 },
-    { id: 3, username: 'Lannister', email: 'Jaime', is_staff: 45 },
-    { id: 4, username: 'Stark', email: 'Arya', is_staff: 16 },
-    { id: 5, username: 'Targaryen', email: 'Daenerys', is_staff: null },
-    { id: 6, username: 'Melisandre', email: null, is_staff: 150 },
-    { id: 7, username: 'Clifford', email: 'Ferrara', is_staff: 44 },
-    { id: 8, username: 'Frances', email: 'Rossini', is_staff: 36 },
-    { id: 9, username: 'Roxie', email: 'Harvey', is_staff: 65 },
-  ];
 
 function Patientslist() {
+
+    const [patients, setPatients] = useState([])
 
     useEffect(() => {
         // axios.post(`/api/register`, {
@@ -38,10 +23,26 @@ function Patientslist() {
         axios.get(`/api/patients`, {
             headers: { Authorization: `Token ${localStorage.getItem('token')}`}
         }).then((response) => {
-            console.log(response);
+            setPatients(response.data);
         })
     }, []);
 
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'username', headerName: 'Username', width: 300 },
+        { field: 'email', headerName: 'Email', width: 400 },
+        { field: 'is_staff', headerName: 'Role', width: 100 },
+    ]
+    
+    const rows = patients.map((p) => (
+            {
+                id: p.id,
+                username: p.username,
+                email: p.email,
+                is_staff: p.is_staff
+            }
+        ))
+      ;
 
     
     return (
